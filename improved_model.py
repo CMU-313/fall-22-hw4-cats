24a5889be6c6d5b6c923836b615f4670692e454e
Philip Kaufholz <pkaufhol@andrew.cmu.edu>
2022-11-08 16:26:11 -0500

    changed dependent variable to qual_student

--- improved_model.py
+++ improved_model.py
@@ -8,14 +8,14 @@ import joblib
 df = pd.read_csv('data/student-mat.csv', sep=';')
 
 #definition of Qual student
-#df['qual_student'] = np.where(df['G3']>=15, 1, 0) 
+df['qual_student'] = np.where(df['G3']>=15, 1, 0) 
 
-#include = ['school','reason','failures','activities','higher','absences','G1','G2','qual_student']
-include = ['school','reason','failures','activities','higher','absences','G1','G2','G3']
+include = ['school','reason','failures','activities','higher','absences','G1','G2','qual_student']
+#include = ['school','reason','failures','activities','higher','absences','G1','G2','G3']
 df.drop(columns=df.columns.difference(include), inplace = True) #8 variables
 
 categoricals = []
-for col, col_type in df.dtypes.iteritems():
+for col, col_type in df.dtypes.items():
      if col_type == 'O': #strings are categorical
         if col.isnumeric():  #if it is a number in a string form, get it back to a number
             pd.to_numeric(col)
@@ -24,10 +24,10 @@ for col, col_type in df.dtypes.iteritems():
     
 df_conv = pd.get_dummies(df, columns=categoricals)
 
-print(df_conv)
+#print(df_conv)
      
-#dependent_variable = 'qual_student'
-dependent_variable = 'G3'
+dependent_variable = 'qual_student'
+#dependent_variable = 'G3'
 x = df_conv[df_conv.columns.difference([dependent_variable])]
 y = df_conv[dependent_variable]
 clf = rf(n_estimators = 1000)
